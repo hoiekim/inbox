1b8e781a297daecc44da83bbe06a45269d2f9f87
Hoie Kim <code@hoie.kim>
2020-12-20 09:06:21 +0000

    move domain name to env

--- routes/mails.js
+++ routes/mails.js
@@ -19,41 +19,41 @@ obj.getAttachment = async (req, res) => {
   if (!req.session.admin) return res.json(new Error("Admin Login is required"));
   try {
     const attachment = await db.getAttachment(req.params.id);
-    res.send(attachment);
+    res.status(200).send(attachment);
   } catch (err) {
     console.log(err);
-    res.json(new Error("Failed to get attachment data"));
+    res.status(500).json(new Error("Failed to get attachment data"));
   }
 };
 
 obj.getAccounts = async (req, res) => {
   if (!req.session.admin) return res.json(new Error("Admin Login is required"));
   const accounts = await db.getAccounts();
-  res.json(accounts);
+  res.status(200).json(accounts);
 };
 
 obj.getUnreadNo = async (req, res) => {
   if (!req.session.admin) return res.json(new Error("Admin Login is required"));
   const number = await db.getUnreadNo(req.params.account);
-  res.json(number);
+  res.status(200).json(number);
 };
 
 obj.markRead = async (req, res) => {
   if (!req.session.admin) return res.json(new Error("Admin Login is required"));
   const result = await db.markRead(req.params.id);
-  res.json(result);
+  res.status(200).json(result);
 };
 
 obj.getMails = async (req, res) => {
   if (!req.session.admin) return res.json(new Error("Admin Login is required"));
   const mails = await db.getMails(req.params.account);
-  res.json(mails);
+  res.status(200).json(mails);
 };
 
 obj.getMailContent = async (req, res) => {
   if (!req.session.admin) return res.json(new Error("Admin Login is required"));
   const mail = await db.getMailContent(req.params.id);
-  res.json(mail);
+  res.status(200).json(mail);
 };
 
 obj.deleteMail = async (req, res) => {
@@ -65,7 +65,7 @@ obj.deleteMail = async (req, res) => {
   );
   if (!req.session.admin) return res.json(new Error("Admin Login is required"));
   const result = await db.deleteMail(req.params.id);
-  res.json(result);
+  res.status(200).json(result);
 };
 
 obj.sendMail = async (req, res) => {
@@ -77,7 +77,7 @@ obj.sendMail = async (req, res) => {
   );
   if (!req.session.admin) return res.json(new Error("Admin Login is required"));
   const result = await mail.sendMail(req.body, req.files.attachments);
-  res.json(result);
+  res.status(200).json(result);
 };
 
 module.exports = obj;
