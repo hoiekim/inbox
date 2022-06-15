761aba65a09f1bb01e68cb82de2bfbce2d9c4031
hoiekim <code@hoie.kim>
2022-06-14 21:20:16 -0700

    Fixes static files paths

--- src/server/index.ts
+++ src/server/index.ts
@@ -32,7 +32,7 @@ app.use(express.json({ limit: "50mb" }));
 const domainName = process.env.DOMAIN || "mydomain";
 const port = process.env.PORT || 3004;
 
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "../../build")));
 app.use(express.json());
 app.use(fileupload());
 app.use(
