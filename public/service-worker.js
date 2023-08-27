89f8f909b6b9d9fafb1de0e9f955d959aa720673
hoiekim <github@hoie.kim>
2023-08-26 22:43:27 -0700

    fixes service-worker to wait badge job

--- public/service-worker.js
+++ public/service-worker.js
@@ -13,8 +13,8 @@ self.addEventListener("push", (event) => {
 
   const setBadge = async () => {
     if (badge_count === undefined) return;
-    if (badge_count === 0) self.navigator.clearAppBadge();
-    else self.navigator.setAppBadge(badge_count);
+    if (badge_count === 0) return self.navigator.clearAppBadge();
+    else return self.navigator.setAppBadge(badge_count);
   };
 
   const refresh = () => fetch("/api/push/refresh/" + push_subscription_id);
